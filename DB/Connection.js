cc315595ecbab3b9ecc6f219e319279b0ff69b6e
Hamdi Ellithy <32525648+Hamdi1996@users.noreply.github.com>
2020-01-10 21:46:58 +0200

    Update Connection.js

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URL ="mongodb+srv://dbUser:root@cluster0-pr5p0.mongodb.net/test?retryWrites=true&w=majority";
+const URL ="mongodb+srv://<username>:<password>@cluster0-pr5p0.mongodb.net/test?retryWrites=true&w=majority";
 
 const connectDB=async ()=>{
    await mongoose.connect(URL,{useUnifiedToplogy:true,useNewUrlParser:true});
